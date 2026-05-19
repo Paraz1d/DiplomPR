b834fc5b576246c05286f3b76805c6c17daeea33
Paraz1d <denissyvorov1710@gmail.com>
2026-05-19 09:54:45 +0300

    1

--- AdminTaskSystem/Views/ProfileView.xaml.cs
+++ AdminTaskSystem/Views/ProfileView.xaml.cs
@@ -8,7 +8,5 @@ public partial class ProfileView : UserControl
 {
     public ProfileView() { InitializeComponent(); Loaded += (_, _) => ExecuteLoad(); }
     private void ExecuteLoad() { if (DataContext is ProfileViewModel vm && vm.LoadCommand.CanExecute(null)) vm.LoadCommand.Execute(null); }
-    private void CurrentPasswordBox_OnPasswordChanged(object sender, RoutedEventArgs e) { if (DataContext is ProfileViewModel vm) vm.CurrentPassword = CurrentPasswordBox.Password; }
-    private void NewPasswordBox_OnPasswordChanged(object sender, RoutedEventArgs e) { if (DataContext is ProfileViewModel vm) vm.NewPassword = NewPasswordBox.Password; }
-    private void ConfirmPasswordBox_OnPasswordChanged(object sender, RoutedEventArgs e) { if (DataContext is ProfileViewModel vm) vm.ConfirmPassword = ConfirmPasswordBox.Password; }
+   
 }
